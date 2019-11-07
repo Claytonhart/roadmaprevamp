e4de829e16751be062fbe41eb040ec76e19769a5
Clayton Hart <claytonhart123@yahoo.com>
2019-11-07 03:54:55 -0800

    initial board functionality implemented

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import Project from './components/Project';
 
 function App() {
-  return <div className='App'>app</div>;
+  return (
+    <div className='App'>
+      <Project />
+    </div>
+  );
 }
 
 export default App;
